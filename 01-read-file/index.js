04d5b07b5fa63069267b4c44905de26a2653c2ce
Sherita Sherlene <sheritsh@et-g5.kzn.21-school.ru>
2023-05-08 18:56:48 +0300

    solution: 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,2 +1,7 @@
 const fs = require('fs');
 const path = require('path');
+const { stdout, stderr } = require('process');
+
+const readableStream = fs.createReadStream(path.resolve(__dirname, './text.txt'));
+readableStream.on('data', (chunk) => stdout.write(chunk));
+readableStream.on('error', (err) => stderr.write(err.message));
