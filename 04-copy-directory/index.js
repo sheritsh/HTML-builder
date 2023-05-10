23bc5434f4e2664e102ebe5e65d909d1e0f5d366
Sherita Sherlene <sheritsh@et-g5.kzn.21-school.ru>
2023-05-10 03:28:43 +0300

    fix 04-task: now when copying files  -> delete already created files

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,22 +4,26 @@ const { stdout } = require('process');
 
 const dirPath = path.resolve(__dirname, 'files');
 
-fs.mkdir(path.resolve(__dirname, 'files-copy'), { recursive: true }, (err) => {
+fs.rm(path.resolve(__dirname, 'files-copy'), { recursive: true, force: true }, (err) => {
   if (err) throw err;
 
-  fs.readdir(dirPath, { withFileTypes: true }, (err, files) => {
+  fs.mkdir(path.resolve(__dirname, 'files-copy'), { recursive: true }, (err) => {
     if (err) throw err;
-    files.forEach((file) => {
-      if (file.isFile()) {
-        fs.copyFile(
-          path.resolve(__dirname, 'files', file.name),
-          path.resolve(__dirname, 'files-copy', file.name),
-          (err) => {
-            if (err) throw err;
-          }
-        );
-      }
+
+    fs.readdir(dirPath, { withFileTypes: true }, (err, files) => {
+      if (err) throw err;
+      files.forEach((file) => {
+        if (file.isFile()) {
+          fs.copyFile(
+            path.resolve(__dirname, 'files', file.name),
+            path.resolve(__dirname, 'files-copy', file.name),
+            (err) => {
+              if (err) throw err;
+            }
+          );
+        }
+      });
+      stdout.write('All files copied successfully\n');
     });
-    stdout.write('All files copied successfully\n');
   });
 });
